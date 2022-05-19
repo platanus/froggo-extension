03f605f281af86d69f9ac53acbb1b85c975c9161
Álvaro Tobar <ankaph@protonmail.com>
2022-05-19 11:03:44 -0400

    chore(): configure tailwind colors, css purge and style setup script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,61 @@
+/* eslint-disable no-undef */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.erb',
+    ],
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'body': ['Open Sans', 'sans-serif'],
+        'landing': ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        teal: colors.teal,
+        froggo: {
+          black: '#0e131f',
+          blue: {
+            DEFAULT: '#38405f',
+            dark: '#232943',
+          },
+          gray: {
+            DEFAULT: '#828282',
+            light: '#d5d4d9',
+          },
+          green: {
+            DEFAULT: '#27a594',
+            '300': '#5ad8c7',
+            '100': '#adece3',
+          },
+          red: {
+            DEFAULT: '#ff1747',
+            '300': '#ff7d98',
+            '100': '#ffe3e9',
+          },
+          yellow: {
+            DEFAULT: '#f5cb5c',
+            '300': '#f9dd96',
+            '100': '#fcf0cf',
+          },
+        },
+      },
+      height: {
+        '13': '52px',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['disabled'],
+      cursor: ['disabled'],
+    },
   },
   plugins: [],
 };
