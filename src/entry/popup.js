db0e1f48918b0ea83311d23bc9dfd8b741e1b2ed
Álvaro Tobar <ankaph@protonmail.com>
2022-05-20 16:12:17 -0400

    feat(popup): add vue-select and moment to popup entry

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import VSelect from 'vue-select';
+import 'vue-select/dist/vue-select.css';
+import moment from 'moment';
 import store from '../store';
 import App from '../view/popup.vue';
 
 Vue.config.productionTip = false;
+
+Vue.component('v-select', VSelect);
+
+moment.locale('es');
+Vue.filter('toRelative', (time) => moment(time).fromNow());
 
 new Vue({
   store,
