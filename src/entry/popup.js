15ed46e7c4eaecb1cd1ffec7853fb64a9fb42dc6
Álvaro Tobar <ankaph@protonmail.com>
2022-05-26 22:01:01 -0400

    chore(): add floating-vue

--- src/entry/popup.js
+++ src/entry/popup.js
@@ -2,12 +2,15 @@ import Vue from 'vue';
 import VSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
 import moment from 'moment';
+import { Tooltip } from 'floating-vue';
+import 'floating-vue/dist/style.css';
 import store from '../store';
 import App from '../view/popup.vue';
 
 Vue.config.productionTip = false;
 
 Vue.component('v-select', VSelect);
+Vue.component('v-tooltip', Tooltip);
 
 moment.locale('es');
 Vue.filter('toRelative', (time) => moment(time).fromNow());
